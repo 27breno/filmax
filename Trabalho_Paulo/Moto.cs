d91cc8c8fa049c6b217df71b7b9e4e866f14b16c
duda-1 <163910040+pedrofelipe09@users.noreply.github.com>
2024-07-01 16:47:56 -0300

    Create Moto.cs

---
+++ Trabalho_Paulo/Moto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalho_Paulo
+{
+    public class Moto
+    {
+        public int Cilindrada { get; set; }
+        public int Ano { get; set; }
+        public string Marca { get; set; }
+    }
+}
