34e601271b05a73a20e0086c952a0f1c3f36f88e
duda-1 <163910040+pedrofelipe09@users.noreply.github.com>
2024-07-01 16:54:17 -0300

    Create HistoricoVagas.cs

---
+++ Trabalho_Paulo/HistoricoVagas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalho_Paulo
+{
+    public class HistoricoVagas
+    {
+        public int Id { get; set; }
+        public double DataDeAtualizacao { get; set; }
+        public int IdVaga { get; set; }
+        public string Objeto { get; set; }
+
+    }
+}
