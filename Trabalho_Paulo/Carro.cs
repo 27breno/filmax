26219efa55f431c81450fc600ce44902889568d5
duda-1 <163910040+pedrofelipe09@users.noreply.github.com>
2024-07-01 16:43:21 -0300

    Classe Carro

---
+++ Trabalho_Paulo/Carro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalho_Paulo
+{
+    public class Carro
+    {
+        public int QuantidadePortas { get; set; }
+        public int Ano { get; set; }
+        public string Marca { get; set; }
+
+    }
+
+    
+}
